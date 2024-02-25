bcb28eddd9ac835bdf8ceac76a9d74b17380a0aa
Yash Kathe <yashkathe123@gmail.com>
2024-02-24 20:43:50 -0800

    created and styled Project sidebar, and newReminder component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import NewReminder from "./components/NewReminder";
+import ProjectSidebar from "./components/ProjectSidebar";
+
 function App() {
-  return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+	return (
+		<main className="h-screen my-8 flex gap-8">
+			<ProjectSidebar />
+            <NewReminder/>
+		</main>
+	);
 }
 
 export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Input = ({ label, isTextArea, ...props }) => {
+	return (
+		<p className='flex flex-col gap-1 my-4'>
+			<label className='text-sm font-bold uppercase text-stone-500'>
+				{label}
+			</label>
+			{isTextArea ? (
+				<textarea
+					className='w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
+					{...props}
+				/>
+			) : (
+				<input
+					className='w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
+					{...props}
+				/>
+			)}
+		</p>
+	);
+};
+
+export default Input;
